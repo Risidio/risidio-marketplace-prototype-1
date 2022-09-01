7515d628a8e7bd3592e878501bf31960799a2ca4
Apower11 <59498034+Apower11@users.noreply.github.com>
2022-09-01 11:29:19 +0100

    deploy

---
+++ precache-manifest.8035e6015b81738557485db30a7fd33b.js
@@ -0,0 +1,262 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3adcb46728ff2ff8e77",
+    "url": "/risidio-marketplace-prototype-1/css/app.6739bc43.css"
+  },
+  {
+    "revision": "f9661a00e103a4f9e002",
+    "url": "/risidio-marketplace-prototype-1/css/chunk-0151b3bd.e769faee.css"
+  },
+  {
+    "revision": "f2fdb34b31e3339d887c",
+    "url": "/risidio-marketplace-prototype-1/css/chunk-0df9b5ab.ce93ef47.css"
+  },
+  {
+    "revision": "2268f98edb0a6d38c41f",
+    "url": "/risidio-marketplace-prototype-1/css/chunk-258a5efe.55c7855a.css"
+  },
+  {
+    "revision": "f8feba880e7fad146ab4",
+    "url": "/risidio-marketplace-prototype-1/css/chunk-34eb698c.420baf0c.css"
+  },
+  {
+    "revision": "0ae515804f5092dfd9bb",
+    "url": "/risidio-marketplace-prototype-1/css/chunk-3b50d98a.d2278819.css"
+  },
+  {
+    "revision": "b6a032e5742b7bec9531",
+    "url": "/risidio-marketplace-prototype-1/css/chunk-46580c8e.a8aa2a08.css"
+  },
+  {
+    "revision": "c97d2ba16a8949258ce0",
+    "url": "/risidio-marketplace-prototype-1/css/chunk-486a1195.372bc52f.css"
+  },
+  {
+    "revision": "a0fc89ab723f3b91fbd9",
+    "url": "/risidio-marketplace-prototype-1/css/chunk-510cc591.af3628dd.css"
+  },
+  {
+    "revision": "133dd53a12afb1aff5d8",
+    "url": "/risidio-marketplace-prototype-1/css/chunk-522d82d9.cb471d6e.css"
+  },
+  {
+    "revision": "700146c9c5cf48755223",
+    "url": "/risidio-marketplace-prototype-1/css/chunk-6aa82058.b8719002.css"
+  },
+  {
+    "revision": "b68ffdd4a116b33180e4",
+    "url": "/risidio-marketplace-prototype-1/css/chunk-729d3ca1.1dd01d1f.css"
+  },
+  {
+    "revision": "523d35ba0c25b197b4bd",
+    "url": "/risidio-marketplace-prototype-1/css/chunk-7728d686.4a0e4f3c.css"
+  },
+  {
+    "revision": "7f3c46f2bc5f6dad7cf7",
+    "url": "/risidio-marketplace-prototype-1/css/chunk-7e4d4576.b2b45218.css"
+  },
+  {
+    "revision": "c89ffd15a2a1723ee43b",
+    "url": "/risidio-marketplace-prototype-1/css/chunk-c3498e94.f00736d1.css"
+  },
+  {
+    "revision": "15de32f3b71bbb8b8ca3",
+    "url": "/risidio-marketplace-prototype-1/css/chunk-dbc703c4.373c98b4.css"
+  },
+  {
+    "revision": "6117711591d1759e0429",
+    "url": "/risidio-marketplace-prototype-1/css/chunk-vendors.55204a1e.css"
+  },
+  {
+    "revision": "7ff8eb1322a378e2ef519496a00c23f9",
+    "url": "/risidio-marketplace-prototype-1/img/404.7ff8eb13.png"
+  },
+  {
+    "revision": "205d374579db4ebdad7714a0a3abd8c8",
+    "url": "/risidio-marketplace-prototype-1/img/coming-soon.205d3745.png"
+  },
+  {
+    "revision": "cd6a1804fe6ec7b183e1bed2b6983b4f",
+    "url": "/risidio-marketplace-prototype-1/img/gridDisplay.cd6a1804.svg"
+  },
+  {
+    "revision": "56b640a37644d01b425caea1569dcd3a",
+    "url": "/risidio-marketplace-prototype-1/img/indige-40.56b640a3.jpg"
+  },
+  {
+    "revision": "0e7ad593903d06a33a7c9582f16aaa90",
+    "url": "/risidio-marketplace-prototype-1/img/indige-60.0e7ad593.jpg"
+  },
+  {
+    "revision": "d8a3e4a882f8027533fed0528aee0600",
+    "url": "/risidio-marketplace-prototype-1/img/indige.d8a3e4a8.jpg"
+  },
+  {
+    "revision": "ab0175de005f4bbad2edf2d10e1020d6",
+    "url": "/risidio-marketplace-prototype-1/img/loading-risid.ab0175de.gif"
+  },
+  {
+    "revision": "83028c2a0978914391397cd6c153adb3",
+    "url": "/risidio-marketplace-prototype-1/img/loading2.83028c2a.gif"
+  },
+  {
+    "revision": "9230413b1b9f3e3d50c86f4b4be4cb65",
+    "url": "/risidio-marketplace-prototype-1/img/mailIcon.9230413b.svg"
+  },
+  {
+    "revision": "71bac63b0e6345cb1e8b83803a39ab07",
+    "url": "/risidio-marketplace-prototype-1/img/normalDisplay.71bac63b.svg"
+  },
+  {
+    "revision": "84723b15b66148189bf9ac89bbe3465f",
+    "url": "/risidio-marketplace-prototype-1/img/open-in-new-icon.84723b15.svg"
+  },
+  {
+    "revision": "25e22e7ca4de34a567372510e3d27708",
+    "url": "/risidio-marketplace-prototype-1/img/paint-palette-risidio.25e22e7c.png"
+  },
+  {
+    "revision": "0ede223a068e5c9376158990d3af87ab",
+    "url": "/risidio-marketplace-prototype-1/img/risidio.0ede223a.svg"
+  },
+  {
+    "revision": "2cf7a7741b94ac6b7db2af1fc09a37f4",
+    "url": "/risidio-marketplace-prototype-1/img/risidio_white_logo.2cf7a774.svg"
+  },
+  {
+    "revision": "8217b8f794e2bb7ac4f19ba5a256beba",
+    "url": "/risidio-marketplace-prototype-1/img/sticksnstones_logo.8217b8f7.png"
+  },
+  {
+    "revision": "1ae1282770dcf95a6b1fc71bd9b8b4aa",
+    "url": "/risidio-marketplace-prototype-1/img/triangle-and-painter.1ae12827.png"
+  },
+  {
+    "revision": "306009719073a1a25d31896d99c32270",
+    "url": "/risidio-marketplace-prototype-1/index.html"
+  },
+  {
+    "revision": "c3adcb46728ff2ff8e77",
+    "url": "/risidio-marketplace-prototype-1/js/app.0bf3375a.js"
+  },
+  {
+    "revision": "f9661a00e103a4f9e002",
+    "url": "/risidio-marketplace-prototype-1/js/chunk-0151b3bd.aef1ad98.js"
+  },
+  {
+    "revision": "f2fdb34b31e3339d887c",
+    "url": "/risidio-marketplace-prototype-1/js/chunk-0df9b5ab.65f55738.js"
+  },
+  {
+    "revision": "2268f98edb0a6d38c41f",
+    "url": "/risidio-marketplace-prototype-1/js/chunk-258a5efe.5b4b6724.js"
+  },
+  {
+    "revision": "897ccca8cd2204194129",
+    "url": "/risidio-marketplace-prototype-1/js/chunk-2d0aacbc.6c228f66.js"
+  },
+  {
+    "revision": "16f96c1ce229ecb7aa3c",
+    "url": "/risidio-marketplace-prototype-1/js/chunk-2d225278.0d3686b2.js"
+  },
+  {
+    "revision": "f8feba880e7fad146ab4",
+    "url": "/risidio-marketplace-prototype-1/js/chunk-34eb698c.d9c8c8ee.js"
+  },
+  {
+    "revision": "0ae515804f5092dfd9bb",
+    "url": "/risidio-marketplace-prototype-1/js/chunk-3b50d98a.0ebd30ce.js"
+  },
+  {
+    "revision": "5ae92c3c9c7be494f612",
+    "url": "/risidio-marketplace-prototype-1/js/chunk-42278f09.b26eed63.js"
+  },
+  {
+    "revision": "b6a032e5742b7bec9531",
+    "url": "/risidio-marketplace-prototype-1/js/chunk-46580c8e.067420bd.js"
+  },
+  {
+    "revision": "c97d2ba16a8949258ce0",
+    "url": "/risidio-marketplace-prototype-1/js/chunk-486a1195.8cdb6441.js"
+  },
+  {
+    "revision": "a0fc89ab723f3b91fbd9",
+    "url": "/risidio-marketplace-prototype-1/js/chunk-510cc591.f1188b7f.js"
+  },
+  {
+    "revision": "133dd53a12afb1aff5d8",
+    "url": "/risidio-marketplace-prototype-1/js/chunk-522d82d9.dbf49171.js"
+  },
+  {
+    "revision": "700146c9c5cf48755223",
+    "url": "/risidio-marketplace-prototype-1/js/chunk-6aa82058.b8aebce0.js"
+  },
+  {
+    "revision": "b68ffdd4a116b33180e4",
+    "url": "/risidio-marketplace-prototype-1/js/chunk-729d3ca1.df456088.js"
+  },
+  {
+    "revision": "523d35ba0c25b197b4bd",
+    "url": "/risidio-marketplace-prototype-1/js/chunk-7728d686.32f5d701.js"
+  },
+  {
+    "revision": "7f3c46f2bc5f6dad7cf7",
+    "url": "/risidio-marketplace-prototype-1/js/chunk-7e4d4576.86bfb42f.js"
+  },
+  {
+    "revision": "c89ffd15a2a1723ee43b",
+    "url": "/risidio-marketplace-prototype-1/js/chunk-c3498e94.b95512ff.js"
+  },
+  {
+    "revision": "15de32f3b71bbb8b8ca3",
+    "url": "/risidio-marketplace-prototype-1/js/chunk-dbc703c4.45973885.js"
+  },
+  {
+    "revision": "6117711591d1759e0429",
+    "url": "/risidio-marketplace-prototype-1/js/chunk-vendors.09750d7e.js"
+  },
+  {
+    "revision": "4e7cbbe32c1752c9ccc6a04b99292663",
+    "url": "/risidio-marketplace-prototype-1/manifest.json"
+  },
+  {
+    "revision": "f494f6d3d73167d385709b71d7e65881",
+    "url": "/risidio-marketplace-prototype-1/risidio-16x16.png"
+  },
+  {
+    "revision": "aa473276a03e6d642843da56beb5ef4e",
+    "url": "/risidio-marketplace-prototype-1/risidio-256x256.png"
+  },
+  {
+    "revision": "150d99775b604eff72f8709290c25dd2",
+    "url": "/risidio-marketplace-prototype-1/risidio-32x32.png"
+  },
+  {
+    "revision": "fb5bd600892e510fe967b5468485dfcc",
+    "url": "/risidio-marketplace-prototype-1/risidio-512x512.png"
+  },
+  {
+    "revision": "35a62b83d6f286ddd4931b3cd07de01f",
+    "url": "/risidio-marketplace-prototype-1/risidio-64x64.png"
+  },
+  {
+    "revision": "18ea3b3ff719df62528798d7a92f7ddf",
+    "url": "/risidio-marketplace-prototype-1/risidio-96x96.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/risidio-marketplace-prototype-1/robots.txt"
+  },
+  {
+    "revision": "8217b8f794e2bb7ac4f19ba5a256beba",
+    "url": "/risidio-marketplace-prototype-1/sticksnstones_logo.png"
+  },
+  {
+    "revision": "9bab72083727d97e07d5d860c7d49969",
+    "url": "/risidio-marketplace-prototype-1/styles/javascript-not-enabled.css"
+  },
+  {
+    "revision": "b8f5d4a0dc17115555e037001a7d4580",
+    "url": "/risidio-marketplace-prototype-1/styles/javascript-not-enabled.scss"
+  }
+]);
